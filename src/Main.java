a75b878b6a41e6480c0ce35392b7af9997dad3f1
Kexin2000 <3299133282@qq.com>
2024-04-01 19:57:43 +0800

    3

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
     exampleMethod(1, "Example String", 3.14, true, 'c', 123456789012345L, 2.718f, (byte) 127);
   }
 
-  public static void exampleMethod(int param1, String param2, double param3, boolean param4, char param5, long param6, float param7, byte param8) {
+  public static void exampleMethod(int param1, String param2, double param3, boolean param4,
+                                   char param5, long param6, float param7, byte param8) {
         // This is the method body. You can add your logic here.
         System.out.println("Integer parameter: " + param1);
         System.out.println("String parameter: " + param2);
